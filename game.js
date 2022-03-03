6bfababb71dcd3baa63ca075a154177991dd47c1
Jacob Lavarine <Jlavarine6237@gmail.com>
2022-03-03 12:18:42 -0600

    Setup player class

--- game.js
+++
@@ -1,6 +0,0 @@
-class Game {
-  constructor() {
-    
-  }
-};
-module.exports = Game;
--- player.js
+++ player.js
@@ -1,11 +1,15 @@
 class Player {
-  constructor() {
-    this.name = 'human'
-    this.token = '👨'
-    this.wins = 0
+  constructor({playerObj}) {
+    this.name = playerObj.name;
+    this.token = playerObj.token;
+    this.wins = 0;
+    this.selectedChoice = ;
+  };
+  selectComputerChoice() {
+
   };
   takeTurn() {
-    
+
   };
 };
 module.exports = Player;
