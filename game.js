814a28e29474bd599e59ff7b740ae9b7274450e9
Jacob Lavarine <Jlavarine6237@gmail.com>
2022-03-06 10:11:34 -0600

    Reset board after displaying winner functionality

--- game.js
+++ game.js
@@ -95,7 +95,6 @@ class Game {
   resetBoard() {
     this.humanPlayerSelectedChoice = '';
     this.computerPlayerSelectedChoice = '';
-    this.selectedGameMode = '';
     console.log('reset')
   };
 };
